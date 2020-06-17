e8c4ebc46698b065e0a72e30cc9ba934165c5f98
Pari-Dev <pranitapanigrahi50@gmail.com>
2020-06-17 17:37:31 +0530

    2 files modified, 1 added

--- Card.js
+++ Card.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import Parent from './Parent';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './style.css';
 
 const Card = ({users})=> {
@@ -10,10 +16,12 @@ const Card = ({users})=> {
   <div>
    <img style={{width:'80px',height:'100px',margin:'10px'}}src = "https://www.callsprout.com/wp-content/uploads/2016/05/portrait-placeholder-510x315.jpg"/>
   </div>
+  <Router>
   <div>
-  <h4>{user.name}</h4>
+ <Link to='profileInfo'> <h4>{user.name}</h4></Link>
   <h4 style={{marginLeft:'20px'}}>{user.website}</h4>
   </div>
+  </Router>
   </div>)
  })
   )
